efbf0d56a7fa67a43eaf2dc303a2c3542095a3f2
m00n1ight <kkhohlow@gmail.com>
2020-03-21 20:56:39 +0300

    [Kirill] added config services templates

---
+++ hipmon/history-storage-service/src/test/java/com/hack/hipmon/historystorageservice/HistoryStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hack.hipmon.historystorageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryStorageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

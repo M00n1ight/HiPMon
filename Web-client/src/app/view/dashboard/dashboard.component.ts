0637b801a591f286704063c4c8878c0d7483066a
Dmitrii <Dlob2@yandex.ru>
2020-03-21 20:37:03 +0300

    [Dmitrii] add dashboard

---
+++ Web-client/src/app/view/dashboard/dashboard.component.ts
@@ -0,0 +1,83 @@
+import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {CompactType, DisplayGrid, GridType} from 'angular-gridster2';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Default,
+  encapsulation: ViewEncapsulation.None
+})
+
+export class DashboardComponent implements OnInit {
+
+  constructor() {
+  }
+
+  options = {
+    gridType: GridType.ScrollVertical,
+    compactType: CompactType.None,
+    margin: 10,
+    outerMargin: true,
+    outerMarginTop: null,
+    outerMarginRight: null,
+    outerMarginBottom: null,
+    outerMarginLeft: null,
+    useTransformPositioning: true,
+    mobileBreakpoint: 640,
+    minCols: 15,
+    maxCols: 100,
+    minRows: 1,
+    maxRows: 100,
+    maxItemCols: 100,
+    minItemCols: 1,
+    maxItemRows: 100,
+    minItemRows: 1,
+    maxItemArea: 2500,
+    minItemArea: 1,
+    defaultItemCols: 1,
+    defaultItemRows: 1,
+    fixedColWidth: 105,
+    fixedRowHeight: 105,
+    keepFixedHeightInMobile: false,
+    keepFixedWidthInMobile: false,
+    scrollSensitivity: 10,
+    scrollSpeed: 20,
+    enableEmptyCellClick: false,
+    enableEmptyCellContextMenu: false,
+    enableEmptyCellDrop: false,
+    enableEmptyCellDrag: false,
+    enableOccupiedCellDrop: false,
+    emptyCellDragMaxCols: 50,
+    emptyCellDragMaxRows: 50,
+    ignoreMarginInRow: false,
+    draggable: {
+      enabled: true,
+    },
+    resizable: {
+      enabled: true,
+    },
+    swap: false,
+    pushItems: true,
+    disablePushOnDrag: false,
+    disablePushOnResize: false,
+    pushDirections: {north: true, east: true, south: true, west: true},
+    pushResizeItems: false,
+    displayGrid: DisplayGrid.Always,
+    disableWindowResize: false,
+    disableWarnings: false,
+    scrollToNewItems: false
+  };
+
+  dashboard = [
+    {cols: 2, rows: 1, y: 0, x: 0},
+    {cols: 2, rows: 2, y: 0, x: 2},
+    {cols: 1, rows: 1, y: 0, x: 4},
+    {cols: 1, rows: 1, y: 2, x: 5},
+  ];
+
+
+  ngOnInit(): void {
+  };
+
+}
